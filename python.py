1ab79ac5329d86d1e33675f6f69f87092847b4b1
wenshao <adam890304@gmail.com>
2020-12-07 23:19:50 +0800

    init

--- python.py
+++ python.py
@@ -5,19 +5,19 @@ s=sys.argv[1]
 str1=''
 def coma(s,b):
     num=0
-    if (57>=ord(s)>=48) and (57>=ord(b)>=48):
-        s= chr(ord(s)+1)
-        if s==b:
+    if (57>=ord(s)>=48) and (57>=ord(b)>=48) and (ord(s)==ord(b)):
+        
             num=1
     return num
 def comn(s,b):
     num=0
-    if (90>=ord(s)>=65) and (90>=ord(b)>=65):  
-        s= chr(ord(s)+1)
-        if s==b:
+    if (90>=ord(s)>=65) and (90>=ord(b)>=65) and (ord(s)==ord(b)):  
+       
              num=1
+    elif (122>=ord(s)>=97) and (122>=ord(b)>=97) and (ord(s)==ord(b)):  
+       
+             num=1	
     return num
-
 if re.search('[A-Za-z]',s)==None:
     str1+='至少輸入一個英文 '
 if re.search('[0-9]',s)==None:
@@ -36,6 +36,7 @@ for i in range(len(s)-1):
         if coma(s[i],s[i+1])==1:
             str1+='數字不可連續 '
             break;
+for i in range(len(s)-1):
         if comn(s[i],s[i+1])==1:
             str1+='字母不可連續 '
             break;
